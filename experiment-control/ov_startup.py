4ed026468057ce46748ef6a01d20d1ba53ef383c
Mališa Vučinić <malishav@gmail.com>
2018-12-11 11:55:49 +0100

    Revert "Issue #4: Invoke scons with virtualenv copy of Python"
    This reverts commit a0a0f8309ad6b452b75152eb9cae642eb0ee375d.

--- experiment-control/ov_startup.py
+++ experiment-control/ov_startup.py
@@ -1,16 +1,9 @@
 import subprocess
 import os
-from distutils import spawn as s
-
-# OpenVisualizer (scons) needs to run with sudo privileges.
-# This creates a problem with dependencies if Python is executed within a virtualenv, as using sudo will bypass the environment variables
-# Therefore, we explicitly invoke scons using the Python path from the virtualenv before sudo is invoked
-pythonPath = s.find_executable("python")
-sconsPath = s.find_executable("scons")
 
 ovDir = os.path.join(os.path.dirname(__file__), "..", "openvisualizer")
 
 class OVStartup:
 
 	def start(self):
-		subprocess.Popen(['sudo', pythonPath, sconsPath, 'runweb', '--port=8080', '--testbed=iotlab', '--broker=broker.mqttdashboard.com', '--root=random'], cwd=ovDir, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+		subprocess.Popen(['sudo', 'scons', 'runweb', '--port=8080', '--testbed=iotlab', '--broker=broker.mqttdashboard.com', '--root=random'], cwd=ovDir, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
