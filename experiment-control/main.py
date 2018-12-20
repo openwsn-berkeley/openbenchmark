c603a7b11d8e4bdd5e2b399a11a8d8623facb3f9
bozidars27 <bozho93@gmail.com>
2018-12-20 12:53:52 +0100

    Issue #13: Separates testbed specific and testbed agnostic logic in different classes. The main `Controller` class contains references to all testbed specific classes

--- experiment-control/main.py
+++ experiment-control/main.py
@@ -3,87 +3,107 @@ import ConfigParser
 import os
 import base64
 
+from reservation import IoTLABReservation
+
 from otbox_startup import OTBoxStartup
 from otbox_flash import OTBoxFlash
 from ov_startup import OVStartup
-from reservation import Reservation
 from ov_log_monitor import OVLogMonitor
 
+class Controller(object):
 
-configParser = ConfigParser.RawConfigParser()   
-configFilePath = os.path.join(os.path.dirname(__file__), 'conf.txt')
-configParser.read(configFilePath)
+	CONFIG_FILE = 'conf.txt'
 
-USERNAME = os.environ["user"] if "user" in os.environ else configParser.get('exp-config', 'user')
-PRIVATE_SSH = os.environ["private_ssh"] if "private_ssh" in os.environ else ""
-HOSTNAME = 'saclay.iot-lab.info'
+	def __init__(self):
+		self.configParser = ConfigParser.RawConfigParser()   
+		self.configFilePath = os.path.join(os.path.dirname(__file__), self.CONFIG_FILE)
+		self.configParser.read(self.configFilePath)
 
-EXP_DURATION = 30 #Duration in minutes
-NODES = "saclay,a8,106+107"
+	def add_parser_args(self, parser):
+		parser.add_argument('--action', 
+	        dest       = 'action',
+	        choices    = ['check', 'reserve', 'terminate', 'otbox', 'otbox-flash', 'ov-start', 'ov-monitor'],
+	        required   = True,
+	        action     = 'store'
+		)
+		parser.add_argument('--testbed', 
+	        dest       = 'testbed',
+	        choices    = ['iotlab', 'opentestbed'],
+	        default    = 'iotlab',
+	        action     = 'store'
+		)
+		parser.add_argument('--firmware', 
+	        dest       = 'firmware',
+	        default    = '03oos_openwsn_prog',
+	        action     = 'store'
+		)
 
-FIRMWARE = os.path.join(os.path.dirname(__file__), 'firmware')
-BROKER = configParser.get('exp-config', 'broker')
+	def get_args(self):
+		parser = argparse.ArgumentParser()
+		self.add_parser_args(parser)
+		args = parser.parse_args()
 
-def add_private_key():
-	if PRIVATE_SSH != "":
-		private_ssh_file = os.path.join(os.path.expanduser("~"), ".ssh", "id_rsa")
-
-		private_ssh_decoded = base64.b64decode(PRIVATE_SSH)
-
-		with open(private_ssh_file, "w") as f:
-			f.write(private_ssh_decoded)
+		return {
+			'action'  : args.action,
+			'testbed' : args.testbed,
+			'firmware': args.firmware
+		}
 
 
-def add_parser_args(parser):
-	parser.add_argument('--action', 
-        dest       = 'action',
-        choices    = ['check', 'reserve', 'terminate', 'otbox', 'otbox-flash', 'ov-start', 'ov-monitor'],
-        required   = True,
-        action     = 'store'
-    )
-	parser.add_argument('--testbed', 
-        dest       = 'testbed',
-        choices    = ['iotlab', 'opentestbed'],
-        default    = 'iotlab',
-        action     = 'store'
-	)
-	parser.add_argument('--firmware', 
-        dest       = 'firmware',
-        default    = '03oos_openwsn_prog',
-        action     = 'store'
-	)
+class IoTLAB(Controller):
 
-def get_args():
-	parser = argparse.ArgumentParser()
-	add_parser_args(parser)
-	args = parser.parse_args()
+	def __init__(self):
+		super(IoTLAB, self).__init__()
 
-	return {
-		'action'  : args.action,
-		'testbed' : args.testbed,
-		'firmware': args.firmware
-	}
+		self.USERNAME = os.environ["user"] if "user" in os.environ else self.configParser.get('exp-config', 'user')
+		self.PRIVATE_SSH = os.environ["private_ssh"] if "private_ssh" in os.environ else ""
+		self.HOSTNAME = 'saclay.iot-lab.info'
+
+		self.EXP_DURATION = 30 # Duration in minutes
+		self.NODES = "saclay,a8,106+107" # To be read from scenario config
+
+		self.FIRMWARE = os.path.join(os.path.dirname(__file__), 'firmware')
+		self.BROKER = self.configParser.get('exp-config', 'broker')
+
+		self.reservation = IoTLABReservation(self.USERNAME, self.HOSTNAME, self.EXP_DURATION, self.NODES)
+		self.add_private_key()
+
+	def add_private_key(self):
+		if self.PRIVATE_SSH != "":
+			private_ssh_file = os.path.join(os.path.expanduser("~"), ".ssh", "id_rsa")
+
+			private_ssh_decoded = base64.b64decode(self.PRIVATE_SSH)
+
+			with open(private_ssh_file, "w") as f:
+				f.write(private_ssh_decoded)
+
+
+TESTBEDS = {
+	"iotlab": IoTLAB
+}
 
 def main():
-	args = get_args()
+	controller = Controller()
+
+	args = controller.get_args()
 
 	action = args['action']
 	testbed = args['testbed']
-	firmware = '{0}/{1}'.format(FIRMWARE, args['firmware'])
 
-	add_private_key()
+	testbed = TESTBEDS[testbed]()
+	firmware = '{0}/{1}'.format(testbed.FIRMWARE, args['firmware'])
 
 	print 'Script started'
 	
-	if action == 'check':
-		print 'Checking experiment'
-		Reservation(USERNAME, HOSTNAME).check_experiment()
 	if action == 'reserve':
 		print 'Reserving nodes'
-		Reservation(USERNAME, HOSTNAME).reserve_experiment(EXP_DURATION, NODES)
-	if action == 'terminate':
+		testbed.reservation.reserve_experiment()
+	elif action == 'check':
+		print 'Checking experiment'
+		testbed.reservation.check_experiment()
+	elif action == 'terminate':
 		print 'Terminating experiment'
-		Reservation(USERNAME, HOSTNAME).terminate_experiment()
+		testbed.reservation.terminate_experiment()
 	elif action == 'otbox':
 		print 'Starting OTBox'
 		OTBoxStartup(USERNAME, HOSTNAME, testbed).start()
@@ -96,6 +116,7 @@ def main():
 	elif action == 'ov-monitor':
 		print 'Starting OV log monitoring'
 		OVLogMonitor().start()
+
 
 if __name__ == '__main__':
 	main()
