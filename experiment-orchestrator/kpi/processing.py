aef9416d00efaa58c12f9167351cbade714d599e
bozidars27 <bozho93@gmail.com>
2019-01-23 17:54:26 +0100

    Issue #14: Adds KPI module for receiving and parsing Experiment Performance Events, calculating KPIs and logging

---
+++ experiment-orchestrator/kpi/processing.py
@@ -0,0 +1,111 @@
+import sys
+sys.path.append('..')
+
+import time
+import json
+import threading
+from logger import Logger
+from utils import Utils
+from mqtt_client._condition_object import ConditionObject
+from scenarios.scenario import Scenario
+
+
+class KPIProcessing:
+
+	def __init__(self):
+		self.logger           = Logger.create()
+		self.condition_object = ConditionObject.create()
+		self.cv               = self.condition_object.exp_event_cv
+		self.queue            = self.condition_object.exp_event_queue
+
+		self.buffer           = []   # Array of dictionaries {"eui64": type `string`, "event_payload": type `dict`}
+		self.start_timestamp  = int(round(time.time() * 1000))
+		self.window_size      = 2*60*1000    # 2 minutes expressed in milliseconds
+
+		self.event_to_method  = {
+			"packetSent"               : None,
+			"packetReceived"           : None,
+			"networkFormationCompleted": self._networkFormationTime,
+			"syncronizationCompleted"  : self._synchronizationPhase,
+			"secureJoinCompleted"      : self._secureJoinPhase,
+			"bandwidthAssigned"        : self._bandwidthAssignment,
+			"radioDutyCycleMeasurement": self._radioDutyCycle,
+			"clockDriftMeasurement"    : None
+		}
+
+
+	def start(self):   # Should be started upon startBenchmark command
+		threading.Thread(target=self._epe_monitor).start()
+
+
+	def _epe_monitor(self):
+		while True:
+			if self.queue.empty():
+				self.cv.acquire()
+				self.cv.wait()     # Released when the queue has a new item
+				self.cv.release()
+
+			complete_payload = self.queue.get()
+			self._process_event(complete_payload)
+
+	def _process_event(self, complete_payload):
+		# Should implement logic for event payload processing and updating log file
+		complete_payload['node_id'] = Utils.eui64_to_id[complete_payload['eui64']]
+		self.logger.log('raw', complete_payload)
+		self._kpi_calculate(complete_payload)
+		self.buffer.append(complete_payload)
+
+
+	# Methods for calculating KPI
+	def _kpi_calculate(self, event_obj):
+		self.event_to_method[event_obj['event_payload']['event']](event_obj)
+
+	def _networkFormationTime(self, event_obj):
+		self.logger.log('kpi', {
+				'kpi'      : 'networkFormationTime',
+				'eui64'    : event_obj['eui64'],
+				'node_id'  : event_obj['node_id'],
+				'timestamp': event_obj['event_payload']['timestamp'],
+				'value'    : 1
+			}
+		)
+
+	def _synchronizationPhase(self, event_obj):
+		self.logger.log('kpi', {
+				'kpi'      : 'syncronizationPhase',
+				'eui64'    : event_obj['eui64'],
+				'node_id'  : Utils.eui64_to_id[event_obj['eui64']],
+				'timestamp': event_obj['event_payload']['timestamp'],
+				'value'    : 1
+			}
+		)
+
+	def _secureJoinPhase(self, event_obj):
+		self.logger.log('kpi', {
+				'kpi'      : 'secureJoinPhase',
+				'eui64'    : event_obj['eui64'],
+				'node_id'  : Utils.eui64_to_id[event_obj['eui64']],
+				'timestamp': event_obj['event_payload']['timestamp'],
+				'value'    : 1
+			}
+		)
+
+	def _bandwidthAssignment(self, event_obj):
+		self.logger.log('kpi', {
+				'kpi'      : 'bandwidthAssignment',
+				'node_id'  : Utils.eui64_to_id[event_obj['eui64']],
+				'eui64'    : event_obj['eui64'],
+				'timestamp': event_obj['event_payload']['timestamp'],
+				'value'    : 1
+			}
+		)
+
+	def _radioDutyCycle(self, event_obj):
+		self.logger.log('kpi', {
+				'kpi'      : 'radioDutyCycle',
+				'eui64'    : event_obj['eui64'],
+				'node_id'  : Utils.eui64_to_id[event_obj['eui64']],
+				'timestamp': event_obj['event_payload']['timestamp'],
+				'value'    : event_obj['event_payload']['dutyCycle']
+			}
+		) 
