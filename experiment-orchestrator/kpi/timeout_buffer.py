a06295e0f43632a65e0a63b672ed70681e835a76
bozidars27 <bozho93@gmail.com>
2019-01-25 15:29:52 +0100

    Issue #14: Swaps deque with dictionary for easier lookup and labels the packets with their own packet token. Notifies KPI processing thread of a packet drop.

--- experiment-orchestrator/kpi/timeout_buffer.py
+++ experiment-orchestrator/kpi/timeout_buffer.py
@@ -1,24 +1,43 @@
+import sys
+sys.path.append('..')
+
 import collections
 import threading
+from mqtt_client._condition_object import ConditionObject
 
 class TimeoutBuffer():
 
 	def __init__(self, timeout):
-		self.timeout = timeout
-		self.buffer  = collections.deque()
-		self.lock    = threading.Lock()
+		self.timeout  = timeout
+		self.buffer   = {}
+		self.lock     = threading.Lock()
 
-	def _expire(self):
+		self.condition_object = ConditionObject.create()
+		self.cv_packet_drop   = self.condition_object.packet_drop_cv
+		self.queue_pck_drop   = self.condition_object.packet_drop_queue
+
+	def _expire(self, packet_token):
+		token = ''.join(str(elem) for elem in packet_token)
 		with self.lock:
-			self.buffer.popleft()
+			if self.buffer[token] != None:
+				self.queue_pck_drop.put(self.buffer[token])
+				self.buffer[token] = None
+
+				self.cv_packet_drop.acquire()
+				self.cv_packet_drop.notifyAll()
+				self.cv_packet_drop.release()
+
 
 	def put(self, item):
 		with self.lock:
-			self.buffer.append(item)
-			threading.Timer(self.timeout, self._expire).start()
+			token = ''.join(str(elem) for elem in item['packetToken'])
+			self.buffer[token] = item
+			threading.Timer(self.timeout, self._expire, [token]).start()
 
 	def find(self, packet_token):
-		for item in self.buffer:
-			if item['event_payload']['packetToken'] == packet_token:
-				return item    # Item not deleted from the deque, since it will expire and be removed anyway
+		token = ''.join(str(elem) for elem in packet_token)
+		if token != '' and self.buffer[token] != None:
+			packet = self.buffer[token]
+			self.buffer[token] = None
+			return packet
 		return None
