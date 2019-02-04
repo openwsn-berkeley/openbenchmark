215025632eeb17e15c31f1ac56045c3ec46f480b
bozidars27 <bozho93@gmail.com>
2019-02-05 00:48:45 +0100

    Issue #14: Introduces time instants generator
    The generator class creates the time points in which the nodes should
    send a packet. The class contains methods for generating time instants
    periodically based on either on uniform or Poisson distribution

---
+++ experiment-orchestrator/scenarios/wizard/_generator.py
@@ -0,0 +1,56 @@
+import numpy as np
+import random
+
+
+class Generator:
+
+	def __init__(self, duration):
+		self.exp_duration = duration    # In seconds
+
+
+	def generate(self, node_pool, params):
+		return getattr(self, "_generate_{0}".format(params['traffic_type']))(node_pool, params)
+
+
+	def _generate_periodic(self, node_pool, params):
+		bottom_interval  = params['interval'][0]
+		top_interval     = params['interval'][1]
+		packets_in_burst = params['packets_in_burst']
+		current_instant  = 0
+		sending_points   = []
+
+		while current_instant < self.exp_duration:
+			interval = round(np.random.uniform(bottom_interval, top_interval), 3)
+			current_instant += interval
+			sending_points.append({
+					'time_sec':         current_instant,
+					'destination':      random.choice(node_pool),
+					'packets_in_burst': packets_in_burst
+				})
+
+		return sending_points
+
+
+	def _generate_poisson(self, node_pool, params):
+		mean               = params['mean']               
+		packets_in_burst   = params['packets_in_burst']
+		period             = 3600   # 1h in seconds
+		top_interval       = 0
+		sending_points     = []
+
+		while top_interval < self.exp_duration:
+			bottom_interval  = top_interval
+			top_interval     = bottom_interval + period
+			num_of_packets   = np.random.poisson(mean)
+
+			# Should be replaced with exponential backoff generator
+			current_instants = [round(np.random.uniform(bottom_interval, top_interval), 3) for i in range(0, num_of_packets)]
+
+			for instant in current_instants:
+				sending_points.append({
+						'time_sec': instant,
+						'destination': random.choice(node_pool),
+						'packets_in_burst': packets_in_burst
+					})
+
+		return sending_points
