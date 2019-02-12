afba57dff9123b94c93b9ef56e61027cc07cec89
bozidars27 <bozho93@gmail.com>
2019-02-12 16:33:05 +0100

    Issue #13: Fixes a typo in `start_experiment.sh` and `wizard.sh`

--- experiment-control/helpers/wilab/jfed_cli/start_experiment.sh
+++ experiment-control/helpers/wilab/jfed_cli/start_experiment.sh
@@ -1,3 +1,2 @@
 PATH_TO_FX='/usr/lib/jvm/javafx-sdk-11.0.2/lib'
-/usr/lib/jvm/jdk-11.0.2/bin/java --module-path $PATH_TO_FX --add-modules=javafx.controls,javafx.s
-wing -jar experimenter-cli2.jar --action start_experiment.yml
+/usr/lib/jvm/jdk-11.0.2/bin/java --module-path $PATH_TO_FX --add-modules=javafx.controls,javafx.swing -jar experimenter-cli2.jar --action start_experiment.yml
