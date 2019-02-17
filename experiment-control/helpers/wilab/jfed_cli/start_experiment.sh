80f61836b8fd5ff0aa54b92a863b22f084f69580
bozidars27 <bozho93@gmail.com>
2019-02-17 01:33:01 +0100

    Issue #13: Adds DISPLAY variable export to `jfed_cli` sh files

--- experiment-control/helpers/wilab/jfed_cli/start_experiment.sh
+++ experiment-control/helpers/wilab/jfed_cli/start_experiment.sh
@@ -1,2 +1,3 @@
 PATH_TO_FX='/usr/lib/jvm/javafx-sdk-11.0.2/lib'
+export DISPLAY=":99"
 /usr/lib/jvm/jdk-11.0.2/bin/java --module-path $PATH_TO_FX --add-modules=javafx.controls,javafx.swing -jar experimenter-cli2.jar --action start_experiment.yml
