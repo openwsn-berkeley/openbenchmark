685b9f61bb58996332d769f9a2307e4f4546aa95
Mališa Vučinić <malishav@gmail.com>
2019-03-23 17:19:22 +0100

    Syntax fix in OV launch command.

--- experiment-control/ov_startup.py
+++ experiment-control/ov_startup.py
@@ -6,4 +6,4 @@ ovDir = os.path.join(os.path.dirname(__file__), "..", "openvisualizer")
 class OVStartup:
 
 	def start(self):
-		subprocess.Popen(['sudo', 'scons', 'runweb', '--port=8080', '--testbed=iotlab', '--broker=broker.mqttdashboard.com', '--root=random'], cwd=ovDir, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+		subprocess.Popen(['sudo', 'scons', 'runweb', '--port=8080', '--testbed=iotlab', '--mqttBroker=argus.paris.inria.fr'], cwd=ovDir, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
