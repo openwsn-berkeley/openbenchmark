81f5eb87e86270ea9b7da3ee88ccdb2de6f8bb97
bozidars27 <bozho93@gmail.com>
2019-04-11 14:54:27 +0200

    Issue #38: Adds method for checking data stream upon OV startup

--- experiment-provisioner/ov_startup.py
+++ experiment-provisioner/ov_startup.py
@@ -1,9 +1,16 @@
 import subprocess
 import os
+from mqtt_client import MQTTClient
 
 ovDir = os.path.join(os.path.dirname(__file__), "..", "openvisualizer")
 
 class OVStartup:
 
+	def __init__(self):
+		self.mqtt_client = MQTTClient.instance()
+
 	def start(self):
 		subprocess.Popen(['sudo', 'scons', 'runweb', '--port=8080', '--testbed=iotlab', '--mqttBroker=argus.paris.inria.fr'], cwd=ovDir, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+	def data_stream_check(self):
+		self.mqtt_client.check_data_stream()
