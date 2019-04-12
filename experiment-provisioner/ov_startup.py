7501de3ee6da87f4af12a971d09e21cd80d06bd6
bozidars27 <bozho93@gmail.com>
2019-04-12 17:55:51 +0200

    Issue #38: Invokes correct method for MQTTClient instance creation

--- experiment-provisioner/ov_startup.py
+++ experiment-provisioner/ov_startup.py
@@ -7,7 +7,7 @@ ovDir = os.path.join(os.path.dirname(__file__), "..", "openvisualizer")
 class OVStartup:
 
 	def __init__(self):
-		self.mqtt_client = MQTTClient.instance()
+		self.mqtt_client = MQTTClient.create("iotlab")
 
 	def start(self):
 		subprocess.Popen(['sudo', 'scons', 'runweb', '--port=8080', '--testbed=iotlab', '--mqttBroker=argus.paris.inria.fr'], cwd=ovDir, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
