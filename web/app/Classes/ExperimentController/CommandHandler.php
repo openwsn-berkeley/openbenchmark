cde5ff82d2a93d1dd0c2117ccaf6c0d4436cdf9b
bozidars27 <bozho93@gmail.com>
2019-04-18 16:15:14 +0200

    Issue #39: Adjusts Python commands generated within `CommandHandler`

--- web/app/Classes/ExperimentController/CommandHandler.php
+++ web/app/Classes/ExperimentController/CommandHandler.php
@@ -2,36 +2,44 @@
 
 namespace App\Classes\ExperimentController;
 
+
 class CommandHandler {
 
-    const COMMAND_MAIN = '/home/vagrant/iotlab-exp-auto/main.py';
-    const OV_GUARD_TIME = 20; //A guard time in seconds for the nodes to start sending serial data before running OV
-    const OV_LOG_GUARD_TIME = 5; //A guard time in seconds for the nodes to start sending serial data before running OV
+    const PROVISIONER_MAIN = 'python /home/vagrant/openbenchmark/experiment-provisioner/main.py';
+    const OV_GUARD_TIME    = 20; //A guard time in seconds for the nodes to start sending serial data before running OV
 
-    private $reserve_nodes_cmd = "python " . self::COMMAND_MAIN . " -reserve 2>&1"; //2>&1 added to insure that all output is given as a return of shell_exec function
-    private $otbox_start_cmd = "python " . self::COMMAND_MAIN . " -otbox 2>&1";
-    private $ov_start_cmd = "python " . self::COMMAND_MAIN . " -ov-start 2>&1";
-    private $ov_monitor_cmd = "python " . self::COMMAND_MAIN . " -ov-monitor > /dev/null 2>/dev/null &";
-    private $exp_terminate = "python " . self::COMMAND_MAIN . " -terminate 2>&1";
 
-    function reserve_nodes() {
-        return shell_exec($this->reserve_nodes_cmd);
+    function reserve_nodes($scenario, $testbed) {
+        //2>&1 added to insure that all output is given as a return of shell_exec function
+        $cmd = self::PROVISIONER_MAIN . " --action=reserve --scenario=" . $scenario . " --testbed=" . $testbed . " &>>reserve.txt";
+        return shell_exec($cmd);
     }
 
-    function otbox_start() {
-        return shell_exec($this->otbox_start_cmd);
+    function flash_firmware($firmware) {
+        $cmd = self::PROVISIONER_MAIN . " --action=flash";
+        
+        if ($firmware != null)
+            $cmd .= " --firmware= " . $firmware;
+
+        $cmd .= " &>>flash.txt";
+
+        return shell_exec($cmd);
     }
 
-    function ov_start() {
-        return shell_exec($this->ov_start_cmd);
-    }
+    function start_ov($scenario, $testbed, $simulator) {
+        $cmd = self::PROVISIONER_MAIN . " --action=ov-start --scenario=" . $scenario . " --testbed=" . $testbed;
 
-    function ov_monitor() {
-        return shell_exec($this->ov_monitor_cmd);
+        if ($simulator != null)
+             $cmd .= " --simulator";
+
+         $cmd .= " &>>start_ov.txt";
+
+        return shell_exec($cmd);
     }
 
     function exp_terminate() {
-        return shell_exec($this->exp_terminate);
+        $cmd = self::PROVISIONER_MAIN . " --action=terminate &>>exp_terminate.txt";
+        return shell_exec($cmd);
     }
 
 }
