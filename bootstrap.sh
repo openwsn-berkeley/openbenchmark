b6ab4fc6b5cd99ed068766469667b145cce97c05
Mališa Vučinić <malishav@gmail.com>
2019-05-14 08:42:00 +0200

    Issue #43: Add verbatim copy of openwsn's install inline to fix a typo.

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,11 +14,35 @@ TAG_OV=OV-7
 REPO_COAP=https://github.com/malishav/coap.git
 REPO_OV=https://github.com/malishav/openvisualizer.git
 
+sudo apt-get update
 sudo apt -y install build-essential
 sudo apt -y install git
 
+### Below is a verbatim copy of the OpenWSN install.sh script
+# until a typo in openvisualizer installation gets fixed
+mkdir openwsn
+sudo apt-get install -y git
+cd ./openwsn/
+git clone https://github.com/openwsn-berkeley/openwsn-fw.git
+git clone https://github.com/openwsn-berkeley/openvisualizer.git
+git clone https://github.com/openwsn-berkeley/coap.git
+cd ./openwsn-fw/
+sudo apt-get install -y python-dev
+sudo apt-get install -y scons
+cd ../openvisualizer/
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-tk
+sudo pip install -r requirements.txt
+cd ../coap/
+sudo pip install -r requirements.txt
+sudo apt-get install -y gcc-arm-none-eabi
+sudo apt-get install -y gcc-msp430
+
+cd ../..
+### End of OpenWSN install.sh script copy
+
 wget https://openwsn.atlassian.net/wiki/download/attachments/29196302/install.sh 
-bash install.sh
+#bash install.sh
 rm install.sh
 
 # Update OpenWSN-CoAP with the correct commit name
@@ -27,7 +51,7 @@ git remote add -t $TAG_COAP -f repository $REPO_COAP
 git checkout $TAG_COAP
 
 # Update OpenVisualizer with the correct commit name
-cd ../openwsn/openvisualizer
+cd ../openvisualizer
 git remote add -t $TAG_OV -f repository $REPO_OV
 git checkout $TAG_OV
 
