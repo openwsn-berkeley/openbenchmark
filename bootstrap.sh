5579664a5f917c53f34d0eac500fb499475b1987
bozidars27 <bozho93@gmail.com>
2019-05-24 14:58:25 +0200

    Issue #43: Removes all commands related to the web (already separated to `boostrap_webdev.sh`)

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,144 +6,65 @@ set -e
 # Print each command before executing it
 set -o xtrace
 
+export LC_ALL=en_US.UTF-8
+
 OPENBENCHMARK_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-LARAVEL_ROOT="$OPENBENCHMARK_DIR/web/public"
-GROUP="$( id -gn )"
-INDEX_JS_PATH="$OPENBENCHMARK_DIR/experiment-provisioner/nodejs_websocket/index.js"
-
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get install -y software-properties-common
-
-# Create system swap as PHP installation may fail without it
-sudo fallocate -l 2G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-
-# Install Apache and Apache dependencies
-sudo apt-get -y install apache2
-cd /tmp && wget http://mirrors.kernel.org/ubuntu/pool/multiverse/liba/libapache-mod-fastcgi/libapache2-mod-fastcgi_2.4.7~0910052141-1.2_amd64.deb
-sudo dpkg -i libapache2-mod-fastcgi_2.4.7~0910052141-1.2_amd64.deb; sudo apt install -f
-
-# Install PHP 7.2 and Composer
-sudo apt-get -y install python-software-properties
-LC_ALL=C.UTF-8 sudo add-apt-repository ppa:ondrej/php -y
-sudo apt-get -y update
-sudo apt-get -y install php7.2 php7.2-fpm php7.2-common
-sudo apt-get -y install composer
-
-# PHP extensions
-sudo apt-get -y install php7.2-zip
-sudo apt-get -y install php7.2-pdo
-sudo apt-get -y install php7.2-mbstring
-sudo apt-get -y install php7.2-tokenizer
-sudo apt-get -y install php7.2-xml
-sudo apt-get -y install php7.2-ctype
-sudo apt-get -y install php7.2-json
-sudo apt-get -y install php7.2-mysql
-sudo apt -y install unzip
-
-# Laravel
-cd $OPENBENCHMARK_DIR
-composer global require "laravel/installer"
-composer create-project --prefer-dist laravel/laravel temp "5.6.*"
-cd temp
-cp $OPENBENCHMARK_DIR/system-config/.env .env
-php artisan key:generate
-rm .env.example
-
-# OpenWSN
-cd $OPENBENCHMARK_DIR
-git clone -b develop_COAP-44 https://github.com/malishav/coap.git
+OPENWSN_DIR=$OPENBENCHMARK_DIR/../openwsn
 
 # FIXME private branch, change to the official repo once code is merged
-git clone -b OV-7 --single-branch https://github.com/malishav/openvisualizer.git
+TAG_COAP=develop_COAP-44
+TAG_OV=OV-7
+REPO_COAP=https://github.com/malishav/coap.git
+REPO_OV=https://github.com/malishav/openvisualizer.git
 
-# Python-dev
-sudo apt-get -y install python-dev
-sudo apt-get -y install python-pip
-sudo apt-get -y install gcc
-sudo apt-get -y install scons
+sudo apt-get update
+sudo apt -y install build-essential
+sudo apt -y install git
 
-sudo pip install -U pyopenssl
+### Below is a verbatim copy of the OpenWSN install.sh script
+# until a typo in openvisualizer installation gets fixed
+mkdir $OPENWSN_DIR
+sudo apt-get install -y git
+cd $OPENWSN_DIR
+git clone https://github.com/openwsn-berkeley/openwsn-fw.git
+git clone https://github.com/openwsn-berkeley/openvisualizer.git
+git clone https://github.com/openwsn-berkeley/coap.git
+cd $OPENWSN_DIR/openwsn-fw/
+sudo apt-get install -y python-dev
 
-# OpenVisualizer needs to be launched with sudo because of the TUN interface
-sudo pip install -r $OPENBENCHMARK_DIR/openvisualizer/requirements.txt --ignore-installed
-# OpenBenchmark scripts do not run with sudo
-pip install -r $OPENBENCHMARK_DIR/requirements.txt --user
+# Installing Python pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python get-pip.py
+rm get-pip.py
 
-# Node.js and NPM
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-sudo apt-get install -fy nodejs
+sudo apt-get install -y scons
+cd $OPENWSN_DIR/openvisualizer/
+sudo apt-get install -y python-tk
+sudo pip install -r requirements.txt --ignore-installed
+cd $OPENWSN_DIR/coap/
+sudo pip install -r requirements.txt
+sudo apt-get install -y gcc-arm-none-eabi
+sudo apt-get install -y gcc-msp430
 
-# Link docs directory to public/docs
-cd $OPENBENCHMARK_DIR/web/public
-ln -s $OPENBENCHMARK_DIR/docs/build/html ./docs
-
-#overwrite Laravel project with our code on startup
 cd $OPENBENCHMARK_DIR
-cp -r -n $OPENBENCHMARK_DIR/temp/* $OPENBENCHMARK_DIR/web/
-cp -r -n $OPENBENCHMARK_DIR/temp/.[!.]* $OPENBENCHMARK_DIR/web/
+### End of OpenWSN install.sh script copy
 
-# remove original Laravel base project
-rm -rf $OPENBENCHMARK_DIR/temp
+wget https://openwsn.atlassian.net/wiki/download/attachments/29196302/install.sh 
+#bash install.sh
+rm install.sh
 
-# configure node_modules
-cd $OPENBENCHMARK_DIR/web
-npm install
-npm update
+# Update OpenWSN-CoAP with the correct commit name
+cd $OPENWSN_DIR/coap
+git remote add -t $TAG_COAP -f repository $REPO_COAP
+git checkout $TAG_COAP
 
-#Configure Apache to use PHP7.2-FPM
-sudo a2enmod actions fastcgi alias proxy_fcgi
+# Update OpenVisualizer with the correct commit name
+cd $OPENWSN_DIR/openvisualizer
+git remote add -t $TAG_OV -f repository $REPO_OV
+git checkout $TAG_OV
 
-# overwrite default Apache config files
-sudo cp $OPENBENCHMARK_DIR/system-config/000-default.conf /etc/apache2/sites-enabled/000-default.conf
-sudo dos2unix /etc/apache2/sites-enabled/000-default.conf
-sudo sed -i "s#@LARAVEL_ROOT@#$LARAVEL_ROOT#" /etc/apache2/sites-enabled/000-default.conf
-
-sudo cp $OPENBENCHMARK_DIR/system-config/envvars /etc/apache2/envvars
-sudo dos2unix /etc/apache2/envvars
-sudo sed -i "s#@USER@#$USER#" /etc/apache2/envvars
-sudo sed -i "s#@GROUP@#$GROUP#" /etc/apache2/envvars
-
-sudo cp $OPENBENCHMARK_DIR/system-config/www.conf /etc/php/7.2/fpm/pool.d/www.conf
-sudo dos2unix /etc/php/7.2/fpm/pool.d/www.conf
-sudo sed -i "s#@USER@#$USER#" /etc/php/7.2/fpm/pool.d/www.conf
-sudo sed -i "s#@GROUP@#$GROUP#" /etc/php/7.2/fpm/pool.d/www.conf
-
-sudo chown -R $USER:$USER /var/lib/apache2/fastcgi
-
-# Start node.js as a deamon
-sudo cp $OPENBENCHMARK_DIR/system-config/index.service /lib/systemd/system/index.service
-sudo sed -i "s#@USER@#$USER#" /lib/systemd/system/index.service
-sudo sed -i "s#@INDEX_JS_PATH@#$INDEX_JS_PATH#" /lib/systemd/system/index.service
-sudo systemctl daemon-reload
-sudo systemctl restart index
-
-sudo rm -f $OPENBENCHMARK_DIR/openvisualizer/build/runui/networkEvent.log*
-echo "sudo rm -f $OPENBENCHMARK_DIR/openvisualizer/build/runui/networkEvent.log*" >> ~/.bashrc
-
-sudo rm -f $OPENBENCHMARK_DIR/openvisualizer/build/runui/*.log*
-echo "sudo rm -f $OPENBENCHMARK_DIR/openvisualizer/build/runui/*.log*" >> ~/.bashrc
-
-# compile app.js and app.css for development
-npm run dev
-
-# generate the docs
-cd $OPENBENCHMARK_DIR/docs
-make html
-
-# install MySQL
-if [ ! -z "$1" ] && [ "$1" = "-mysql" ]
-then
-     sudo apt-get install -y mysql-server
-     mysql -u root -p -e 'CREATE USER "openbenchmark"@"localhost" IDENTIFIED BY "openbenchmark";'
-     mysqladmin -u root -p create openbenchmark
-     mysql -u root -p -e 'GRANT ALL PRIVILEGES ON openbenchmark.* TO "openbenchmark"@"localhost";'
-     cd $OPENBENCHMARK_DIR/web
-     php artisan migrate
-fi
+# Install OpenBenchmark requirements; OpenBenchmark scripts do not run with sudo
+pip install -r $OPENBENCHMARK_DIR/requirements.txt --user
 
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 echo "==================================="
@@ -151,7 +72,3 @@ echo "Please publish the following SSH key on any server where automated SSH is 
 cat ~/.ssh/id_rsa.pub
 echo "==================================="
 
-# Restart Apache and PHP7.2-FPM
-sudo a2enmod rewrite
-sudo service apache2 restart
-sudo service php7.2-fpm restart
