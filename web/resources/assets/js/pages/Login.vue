f01118eb3ae02cce5f494cec1056e772bc40558c
bozidars27 <bozho93@gmail.com>
2019-06-03 13:04:49 +0200

    Adds login page and binds it to `login` route

---
+++ web/resources/assets/js/pages/Login.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="parent row">
+        <sidebar></sidebar>
+    </div>
+</template>
+
+<script>
+    import Sidebar from './../components/reusables/Sidebar.vue'
+
+    let thisComponent;
+    let socketConnected = false;
+
+    export default {
+        components: {
+            Sidebar
+        },
+
+        data: function () {
+            return {
+                dataFlowStarted: true,
+                sidebarScroll: false,
+                root: document.getElementById('root'),
+                threshold: 0.05
+            }
+        },
+
+        methods: {
+
+        },
+
+        mounted() {
+
+        },
+
+        created() {
+            thisComponent = this;
+        }
+    }
+</script>
+
+<style src="./../../sass/layout/margins.css"></style>
+<style src="./../../sass/layout/paddings.css"></style>
+<style src="./../../sass/layout/positioning.css"></style>
+<style src="./../../sass/styles/text-styles.css"></style>
+<style src="./../../sass/styles/buttons.css"></style>
+<style src="./../../sass/styles/colors.css"></style>
+<style src="./../../sass/styles/animations.css"></style>
+<style src="./../../sass/styles/cards.css"></style>
+
+<style scoped>
+    .parent {
+        position: relative;
+    }
+</style>
--- web/resources/assets/js/router/routes.js
+++ web/resources/assets/js/router/routes.js
@@ -1,9 +1,10 @@
 import Home from './../pages/Home.vue';
+import Login from './../pages/Login.vue';
 
 const routes = [
 	{ path: '/', component: Home},
-    { path: '/:id', component: Home, props: true },
     { path: '/home/:id', component: Home, props: true },
+    { path: '/login', component: Login}
 ];
 
 export default routes;
