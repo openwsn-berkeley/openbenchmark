756dbb7a62cafaa75a32c7691df0b2b1ebda467d
bozidars27 <bozho93@gmail.com>
2019-06-10 13:27:50 +0200

    Issue #54: Defines extensions for `Date` and `Number` object

--- web/resources/assets/js/app.js
+++ web/resources/assets/js/app.js
@@ -25,6 +25,34 @@ Vue.directive('observe-visibility', ObserveVisibility);
 
 Vue.use(VModal)
 
+// Date class extension
+Object.defineProperty(Date.prototype, "getCurrentDate", {
+	value: function getCurrentDate(timeOnly = true) {
+		let day   = this.getDate().pad()
+		let month = (this.getMonth() + 1).pad() // getMonth() is zero-based
+		let year  = this.getFullYear()
+		let hours = this.getHours().pad()
+		let mins  = this.getMinutes().pad()
+		let secs  = this.getSeconds().pad()
+
+		if (timeOnly)
+			return hours + ":" + mins + ":" + secs;
+		return day + "." + month + "." + year + " " + hours + ":" + mins + ":" + secs;
+	},
+	writeable: true,
+	configurable: true
+})
+
+Object.defineProperty(Number.prototype, "pad", {
+	value: function(size = 2) {
+		let mString = String(this)
+	    while (mString.length < (size || 2)) {mString = "0" + mString;}
+	    return mString
+	},
+	writeable: true,
+	configurable: true
+})
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
