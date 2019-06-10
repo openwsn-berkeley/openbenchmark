f97d014aefea9b4e84b8570f13d03319a890ec47
bozidars27 <bozho93@gmail.com>
2019-06-10 13:54:35 +0200

    Issue #54: Loads debug data from the MQTT messages

--- web/resources/assets/js/components/landing/DebugWindow.vue
+++ web/resources/assets/js/components/landing/DebugWindow.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="parent col">
 		<div class="row" :class="{gray: ind%2===0}" v-for="(item, ind) in outputs">
-			<span class="debug-row col-1 tag">{{item.tag}}</span>
+			<span class="debug-row col-2 tag">{{item.tag}}</span>
 			<span class="debug-row col-7 message">{{item.message}}</span>
 			<span class="debug-row col-1 time">{{item.time}}</span>
 		</div>
@@ -9,32 +9,49 @@
 </template>
 
 <script>
+	let thisComponent;
+
 	export default {
 		data: function() {
 			return {
 				outputs: [
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"},
-					{tag: "TAG", message: "Message of the debug output", time: "15:02:15"}
+					// Item example {tag: "TAG", message: "Message of the debug output", time: "15:02:15"}
 				]
 			}
+		},
+
+		methods: {
+			parseMqttEvent(payload) {
+				try {
+					let payloadObj = JSON.parse(payload)
+	                
+					let tag = payloadObj["action"]
+					let logEntry = payloadObj["log_entry"]
+					let time = new Date().getCurrentDate();
+
+					this.outputs.push({
+						"tag": tag,
+						"message": logEntry,
+						"time": time
+					})
+
+				} catch (exception) {
+					console.log(exception)
+				}
+			},
+		},
+
+		created() {
+			thisComponent = this
+		},
+
+		mounted() {
+			this.$eventHub.$on("openbenchmark/1/debug", payload => {
+				thisComponent.parseMqttEvent(payload);
+			});
 		}
 	}
+
 </script>
 
 
