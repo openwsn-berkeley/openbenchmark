8335112b7b52761f7deb50a604eea26924c3d8fa
bozidars27 <bozho93@gmail.com>
2019-06-12 16:05:13 +0200

    Issue #53: Logs KPIs in `.cache` directory in JSON format

--- experiment-orchestrator/kpi/logger.py
+++ experiment-orchestrator/kpi/logger.py
@@ -3,6 +3,7 @@ sys.path.append("..")
 
 import json
 import os
+import colorama
 
 from mqtt_client.mqtt_client import MQTTClient
 
@@ -29,8 +30,9 @@ class Logger:
 		self.mqtt_client = MQTTClient.create()
 
 		self.logs = {
-			'kpi': os.path.join(os.path.dirname(__file__), 'kpis', 'kpi_{0}.log'.format(self.experiment_id)),
-			'raw': os.path.join(os.path.dirname(__file__), 'raw', 'raw_{0}.log'.format(self.experiment_id))
+			'kpi'      : os.path.join(os.path.dirname(__file__), 'kpis', 'kpi_{0}.log'.format(self.experiment_id)),
+			'raw'      : os.path.join(os.path.dirname(__file__), 'raw', 'raw_{0}.log'.format(self.experiment_id)),
+			'kpi_cache': os.path.join(os.path.dirname(__file__), 'kpis', '.cache', 'cached_kpi_{0}.json'.format(self.experiment_id))
 		}
 
 		self.log_header()
@@ -62,6 +64,7 @@ class Logger:
 
 		if log_type == 'kpi':
 			self.mqtt_client.push_kpi(payload)
+			self.cache_kpi(payload)
 		elif log_type == 'raw':
 			self.mqtt_client.push_raw(payload)
 
@@ -86,3 +89,60 @@ class Logger:
 		with open(self.logs['raw'], 'a') as f:
 			f.write(header)
 			f.write("\n--------------------------------------\n")
+
+		json_form = {
+			"header": {
+				"date"         : self.date,
+				"experiment_id": self.experiment_id,
+				"testbed"      : self.testbed,
+				"firmware"     : self.firmware,
+				"scenario"     : self.scenario
+			},
+			"general_data": {
+
+			},
+			"data": {
+
+			}
+		}
+
+		with open(self.logs['kpi_cache'], 'a') as f:
+			f.write(json.dumps(json_form))
+
+
+	def cache_kpi(self, payload):
+		#try:
+		with open(self.logs['kpi_cache'], 'r') as f:
+			json_obj = json.loads(f.read())
+					
+			if "node_id" in payload:
+				node_id   = payload["node_id"]
+				kpi       = payload["kpi"]
+				timestamp = payload["timestamp"]
+				value     = payload["value"]
+				
+				if node_id not in json_obj["data"]:
+					json_obj["data"][node_id] = {}
+
+				if kpi not in json_obj["data"][node_id]:
+					json_obj["data"][node_id][kpi] = {}
+					json_obj["data"][node_id][kpi]["timestamp"] = []
+					json_obj["data"][node_id][kpi]["value"]     = []
+
+				json_obj["data"][node_id][kpi]["timestamp"].append(timestamp)
+				json_obj["data"][node_id][kpi]["value"].append(value)
+				
+			else:
+				kpi       = payload["kpi"]
+				timestamp = payload["timestamp"]
+				json_obj["general_data"][kpi] = timestamp
+
+			with open(self.logs['kpi_cache'], 'w') as f:
+					f.write(json.dumps(json_obj))
+		'''
+		except Exception, e:
+			sys.stdout.write("{0}[LOGGER] {1}\n{2}".format(
+				colorama.Fore.RED,
+				str(e), 
+				colorama.Style.RESET_ALL
+			))'''
