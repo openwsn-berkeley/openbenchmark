d55388f3e8918a806d045d19bc06355b49bacdfc
bozidars27 <bozho93@gmail.com>
2019-06-13 16:21:39 +0200

    Issue #53: Defines logs controller

---
+++ web/app/Http/Controllers/LogsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Classes\Logs\LogParser;
+
+
+class LogsController extends Controller
+{
+
+    function __construct() {
+    	$this->log_parser = new LogParser;
+    }
+
+    function get($action, $experiment_id = "") {
+    	return $this->log_parser->get_log_data($action, $experiment_id);
+    }
+
+}
