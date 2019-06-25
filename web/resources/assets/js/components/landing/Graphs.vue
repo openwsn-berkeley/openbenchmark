3b7cba32fbd2f46db4447e3d19cd1b8baa7c6172
bozidars27 <bozho93@gmail.com>
2019-06-25 12:27:20 +0200

    Issue #53: Updates the logic for real-time data parsing. Removes obsolete methods and CSS styles

--- web/resources/assets/js/components/landing/Graphs.vue
+++ web/resources/assets/js/components/landing/Graphs.vue
@@ -125,7 +125,7 @@
                     "node-a8-106": {
                         "latency": {
                             "timestamp":[15388500.87438082,15388500.905358542,15388500.935934938,15388500.966604061,15388500.99736588,15388501.01802762,15388501.04889744],
-                            "value":[0,0,0,0,0,0,0]
+                            "value":[0,0.5,1,0.75,0.85,0.8,0.9]
                         },
                         "reliabilty": {
                             "timestamp":[15388500.987090621],
@@ -139,6 +139,7 @@
         },
 
         methods: {
+            /*** Parse data from the logs ***/
             showModal(name) {
                 this.$modal.show(name)
             },
@@ -148,9 +149,7 @@
             },
 
             loadData() {
-                if (this.experimentId !== undefined) {
-                    this.fetchDataFromLogs()
-                }
+                this.fetchDataFromLogs()
             },
 
             markFileType(fileType) {
@@ -183,161 +182,55 @@
                         
                     });
             },
+            /*** ***/
 
-            relativeTimestampParse(timestamp) {
-                //Converts relative timestamp to milliseconds or seconds since the experiment start
-                //Timestamp in units of timeslots. Each timeslot is 10ms
-                return (timestamp / 1000000).toFixed(2);
-            },
-
-            createBlankDataset() {
-                //Creates a blank dataset for all the existing nodes in the 'nodes' field
-                this.value.nodes.forEach(element => {
-                    this.dataset.push({
-                        id: element.name,
-                        eui64: '',
-                        nodeData: []
-                    })
-                });
-            },
-
-            appendNodeData(id, label, xVal, yVal) {
-                //Appends to 'xAxis' and 'yAxis' of a 'nodeData' (of the node with the given 'eui64') element with the corresponding label
-                //Creates new 'nodeData' element if the label does not exist
+            /*** Parse data from MQTT topic ***/
+            appendNodeData(id, label, timestamp, value) {
                 if (id !== undefined) {
-                    let node = this.getNodeByProperty('id', id);
+                    let newObj = this.clone(this.dataset)
 
-                    console.log("Fetching node: " + id);
-        
-                    let num = node.nodeData.length;
-                    let valueAppended = false;
+                    if ( !(id in newObj) ) 
+                        newObj[id] = {}
 
-                    for (let i=0; i<num; i++) {
-                        if (node.nodeData[i].label === label) {
-                            node.nodeData[i].xAxis.push(xVal);
-                            node.nodeData[i].yAxis.push(yVal);
-
-                            valueAppended = true;
-
-                            if (node.nodeData[i].xAxis.length > this.dataPerChart) {
-                                node.nodeData[i].xAxis.shift();
-                                node.nodeData[i].yAxis.shift();
-                            }
+                    if ( !(label in newObj[id]) )
+                        newObj[id][label] = {
+                            "timestamp": [],
+                            "value"    : []
                         }
-                    }
 
-                    if (!valueAppended) {
-                        node.nodeData.push({
-                            label: label,
-                            xAxis: [xVal],
-                            yAxis: [yVal]
-                        });
-                    }
-                } else {
-                    this.updateGeneralData(label, xVal);
-                }
+                    newObj[id][label]["timestamp"].push(timestamp)
+                    newObj[id][label]["value"].push(value)
+
+                    console.log("Dataset expanded:")
+                    console.log(JSON.stringify(newObj))
+
+                    this.dataset = newObj
+
+                } else   // General data not related to a single node
+                    this.updateGeneralData(label, timestamp);
             },
 
-            updateGeneralData(label, xVal) {
-                let hasElement = false;
-
-                for (let i=0; i<this.generalData.length; i++) {
-                    if (this.generalData[i]["identifier"] === label) {
-                        this.generalData[i]["value"] = xVal;
-                        hasElement = true;
-                        break;    
-                    } 
-                }
-
-                if (!hasElement && this.generalDataTitles.hasOwnProperty(label)) {
-                    this.generalData.push({
-                        "identifier": label,
-                        "value": xVal
-                    });
-                }
-            },
-
-            nodeClick(event, nodeObject) {
-                this.value.nodes.forEach(function(element) {
-                    if (element === nodeObject) {
-                        thisComponent.selectedNode = element;
-                        element._cssClass += " selected";
-                    } else {
-                        element._cssClass = element.defaultCssClass;
-                    }
-                });
-                nodeObject._cssClass += " selected";
-                this.selectNodeData(nodeObject);
-            },
-
-            selectNodeData(node) {
-                //Put dataset element with the given 'id' in 'selectedNode' variable (used for filling the UI with the data)
-                this.selectedNode = node;
-                console.log("Currently showing: " + JSON.stringify(this.getNodeByProperty('id', this.selectedNode)));
-            },
-
-            getNodeByProperty(property, val) {
-                //Searches the dataset for an element with the given property with the given val
-                let num = this.dataset.length;
-                let result = {};
-
-                for (let i=0; i<num; i++) {
-                    if (property === 'id' && this.dataset[i].id === val) {
-                        result = this.dataset[i];
-                    } else if (property === 'eui64' && this.dataset[i].eui64 === val) {
-                        result = this.dataset[i];
-                    }
-                }
-
-                return result;
+            updateGeneralData(label, timestamp) {
+                let newObj = this.clone(this.generalData)
+                newObj[label] = timestamp
+                this.generalData = newObj
+                console.log("General data updated:")
+                console.log(JSON.stringify(this.generalData))
             },
 
             parseLogData(obj) {
-                let id    = obj.node_id;
-                let label = obj.kpi;
-                let xVal  = obj.timestamp;
-                let yVal  = obj.value;
+                let id        = obj.node_id;
+                let label     = obj.kpi;
+                let timestamp = obj.timestamp;
+                let value     = obj.value;
 
-                console.log("Parsing data: " + label + " " + yVal + " id: " + id);
-
-                this.appendNodeData(id, label, this.timestampDiff(xVal), parseFloat(yVal));
-            },
-
-            timestampDiff(stmp) {
-                let timestamp = stmp*1000;
-                console.log("Timestamp: " + this.expStartTimestamp + "; Stmp: " + timestamp);
-                return this.getDuration(new Date(this.expStartTimestamp), new Date(timestamp)).toString();
-            },
-
-            getDuration(d1, d2) {
-                let d3 = new Date(d2 - d1);
-                let d0 = new Date(0);
-
-                return {
-                    getHours: function(){
-                        return d3.getHours() - d0.getHours();
-                    },
-                    getMinutes: function(){
-                        return d3.getMinutes() - d0.getMinutes();
-                    },
-                    getSeconds: function(){
-                        return d3.getSeconds() - d0.getSeconds();
-                    },
-                    getMilliseconds: function() {
-                        return d3.getMilliseconds() - d0.getMilliseconds();
-                    },
-                    toString: function(){
-                        return this.getMinutes() + ":" +
-                            this.getSeconds() + ":" +
-                            this.getMilliseconds();
-                    },
-                };
+                this.appendNodeData(id, label, timestamp, value);
             },
 
             clone(obj) {
-                if (null === obj || "object" !== typeof obj) return obj;
-                let copy = obj.constructor();
-                for (let attr in obj) {
+                if (null == obj || "object" != typeof obj) return obj;
+                var copy = obj.constructor();
+                for (var attr in obj) {
                     if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
                 }
                 return copy;
@@ -353,75 +246,33 @@
                     }
                 }, 1000);  
             },
-            onConnect() {
-                // Once a connection has been made, make a subscription and send a message.
-                //console.log("onConnect");
-                //this.client.subscribe("browser/event");
-                //let message = new Paho.Message("Browser connected to MQTT!");
-                //message.destinationName = "browser/message";
-                //this.client.send(message);
-            },
-            onConnectionLost(responseObject) {
-                //if (responseObject.errorCode !== 0)
-                 //   console.log("onConnectionLost: " + responseObject.errorMessage);
-            },
-            onMessageArrived(message) {
-                //console.log("onMessageArrived: " + message.payloadString);
-            },
 
             parseMqttEvent(payload) {
                 let payloadObj = JSON.parse(payload)
                 let type       = payloadObj["type"]
 
-                if (type == "kpi") {
+                if (type == "kpi") 
                     this.parseLogData(payloadObj["content"])
-                }
             }
+            /*** ***/
 
-        },
-
-        computed:{
-            options() {
-                return {
-                    force: 500,
-                    size: {w:350, h:320},
-                    nodeSize: 20,
-                    nodeLabels: true,
-                    canvas: false
-                }
-            },
-            currentData() {
-                let currentNode = this.getNodeByProperty('id', this.selectedNode.id);
-                return {
-                    id:    currentNode.id,
-                    eui64: currentNode.eui64,
-                    isDag: currentNode.isDag
-                }
-            }
         },
 
         mounted() {
-            this.subscribe();
+            // If `experimentId` doesn't exists accept data from MQTT (real-time charts)
+            // If `experimentId` exists read data from experiment logs
+            if (this.experimentId === undefined)
+                this.subscribe();
+            else
+                this.loadData();
 
-            this.loadData();
-
-            this.$eventHub.$on("MQTT", payload => {
+            this.$eventHub.$on("openbenchmark/1/kpi", payload => {
                 thisComponent.parseMqttEvent(payload);
-            });
-
-            this.$eventHub.$on("NODES_FETCHED", payload => {
-                thisComponent.value = thisComponent.clone(payload);
-                this.createBlankDataset();
-            });
-
-            this.$eventHub.$on("LOG_MODIFICATION", payload => {
-                thisComponent.parseLogData(payload);
             });
         },
 
         created() {
             thisComponent = this;
-            this.expStartTimestamp = new Date().valueOf();
         }
     }
 
@@ -457,52 +308,5 @@
 <style>
     canvas {
         position: relative;
-    }
-</style>
-
-<style>
-    @keyframes loading {
-        0% {fill: rgba(200, 200, 200, .7);}
-        50% {fill: rgba(200, 200, 200, .3);}
-        100% {fill: rgba(200, 200, 200, .7);}
-    }
-
-    .node-off {
-        stroke: rgba(100, 100, 100, .7);
-        fill: rgba(200, 200, 200, .7);
-        stroke-width: 3px;
-        transition: fill .5s ease;
-    }
-    .node-loading {
-        stroke: rgba(100, 100, 100, .7);
-        fill: rgba(200, 200, 200, .7);
-        stroke-width: 3px;
-        transition: fill .5s ease;
-        animation: loading 1s infinite;
-    }
-    .node-on {
-        stroke: rgba(66, 184, 131, 1);
-        fill: rgba(200, 200, 200, .3);
-        stroke-width: 3px;
-        transition: fill .5s ease;
-    }
-    .node-fail {
-        stroke: red;
-        fill: rgba(200, 200, 200, .3);
-        stroke-width: 3px;
-        transition: fill .5s ease;
-    }
-
-    .node-join {
-        stroke: rgba(66, 184, 131, 1);
-        fill: rgba(122,205,168, 1);
-        stroke-width: 3px;
-        transition: fill .5s ease;
-    }
-    .dag-node-join {
-        stroke: orange;
-        fill: rgba(122,205,168, 1);
-        stroke-width: 3px;
-        transition: fill .5s ease;
     }
 </style>
