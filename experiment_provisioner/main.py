8cf7196a796636a74eb2d63a0ac4fdb49d7fc7e3
bozidars27 <bozho93@gmail.com>
2019-07-03 10:23:51 +0200

    Issue #80: Removes `argparse` import and references from `experiment-provisioner/main.py`

--- experiment_provisioner/main.py
+++ experiment_provisioner/main.py
@@ -1,4 +1,3 @@
-import argparse
 import ConfigParser
 import os
 import base64
@@ -37,67 +36,6 @@ class Controller(object):
 		self.COAP_BRANCH = self.configParser.get('dependencies', 'coap-branch')
 		self.OTB_REPO    = self.configParser.get('dependencies', 'otb-repo')
 		self.OTB_TAG     = self.configParser.get('dependencies', 'otb-tag')
-
-	def add_parser_args(self, parser):
-		self.default_fws = {
-			"iotlab": "03oos_openwsn_prog_iotlab",
-			"wilab" : "03oos_openwsn_prog_wilab.ihex"
-		}
-
-		parser.add_argument('--user-id',   # User ID is tied to the OpenBenchmark account
-			dest       = 'user_id',
-			default    = 0,
-			required   = False,
-			action     = 'store'
-		)
-		parser.add_argument('--simulator', 
-			dest       = 'simulator',
-			default    = False,
-			action     = 'store_true'
-		)
-		parser.add_argument('--action', 
-			dest       = 'action',
-			choices    = ['check', 'reserve', 'terminate', 'flash', 'ov-start'],
-			required   = True,
-			action     = 'store'
-		)
-		parser.add_argument('--testbed', 
-			dest       = 'testbed',
-			choices    = ['iotlab', 'wilab'],
-			default    = 'iotlab',
-			action     = 'store'
-		)
-		parser.add_argument('--firmware', 
-			dest       = 'firmware',
-			required   = False,
-			action     = 'store',
-		)
-		parser.add_argument('--branch', 
-			dest       = 'branch',
-			required   = False,
-			action     = 'store',
-		)
-		parser.add_argument('--scenario', 
-			dest       = 'scenario',
-			choices    = ['demo-scenario', 'building-automation', 'home-automation', 'industrial-monitoring'],
-			default    = 'demo-scenario',
-			action     = 'store'
-		)
-
-	def get_args(self):
-		parser = argparse.ArgumentParser()
-		self.add_parser_args(parser)
-		args = parser.parse_args()
-
-		return {
-			'user_id'   : args.user_id,
-			'simulator' : args.simulator,
-			'action'    : args.action,
-			'testbed'   : args.testbed,
-			'firmware'  : args.firmware,
-			'branch'    : args.branch,
-			'scenario'  : args.scenario
-		}
 
 	def _get_duration(self, scenario):
 		config_file = os.path.join(self.SCENARIO_CONFIG, scenario, "_config.json")
