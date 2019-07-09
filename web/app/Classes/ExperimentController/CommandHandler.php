6bf048198ec6df33be1f685496a364767bcdfb8b
bozidars27 <bozho93@gmail.com>
2019-07-09 17:56:32 +0200

    Issue #93: Changes PHP working directory before command execution. Puts all cmd execution logic in a separate private method. Removes unnecessary guard time.

--- web/app/Classes/ExperimentController/CommandHandler.php
+++ web/app/Classes/ExperimentController/CommandHandler.php
@@ -7,8 +7,8 @@ use App\Classes\SuccessResponse;
 
 class CommandHandler {
 
-    const PROVISIONER_MAIN = 'python /home/vagrant/openbenchmark/openbenchmark.py';
-    const OV_GUARD_TIME    = 20; //A guard time in seconds for the nodes to start sending serial data before running OV
+    const WORKING_DIR      = '/home/vagrant/openbenchmark';
+    const PROVISIONER_MAIN = 'python openbenchmark.py';
 
 
     function reserve($user_id, $scenario, $testbed, $async=true) {
@@ -18,9 +18,7 @@ class CommandHandler {
         if ($async)
             $cmd .= " &";
 
-        shell_exec($cmd);
-
-        return SuccessResponse::response(200, $this->get_response_messages($action, $user_id));
+        return $this->_exec_action($action, $cmd, $user_id);
     }
 
     function flash($user_id, $firmware, $async=true) {
@@ -35,9 +33,7 @@ class CommandHandler {
         if ($async)
             $cmd .= " &";
 
-        shell_exec($cmd);
-
-        return SuccessResponse::response(200, $this->get_response_messages($action, $user_id));
+        return $this->_exec_action($action, $cmd, $user_id);
     }
 
     function sut_start($user_id, $scenario, $testbed, $simulator, $async=true) {
@@ -52,19 +48,21 @@ class CommandHandler {
         if ($async)
             $cmd .= " &";
 
-        shell_exec($cmd);
-
-        return SuccessResponse::response(200, $this->get_response_messages($action, $user_id));
+        return $this->_exec_action($action, $cmd, $user_id);
     }
 
     function terminate($user_id) {
         $action = "terminate";
         $cmd = self::PROVISIONER_MAIN . " --user-id=$user_id --action=$action > /dev/null &";
-        shell_exec($cmd);
 
-        return SuccessResponse::response(200, $this->get_response_messages($action, $user_id));
+        return $this->_exec_action($action, $cmd, $user_id);
     }
 
+    private function _exec_action($action, $cmd, $user_id) {
+        chdir(self::WORKING_DIR);
+        shell_exec($cmd);
+        return SuccessResponse::response(200, $this->get_response_messages($action, $user_id));
+    }
 
     private function get_response_messages($action, $user_id) {
         return [
